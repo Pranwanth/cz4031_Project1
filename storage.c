daf6180812b6b0323f3fdf2bef3c135b4b672d91
pranwanth <pranwanth@gmail.com>
2022-09-15 13:06:14 +0800

    record creation and deletion

--- storage.c
+++ storage.c
@@ -7,7 +7,7 @@ const int BLOCK_SIZE = 200;
 
 typedef struct Record {
     int size;
-    char* id;
+    char id[10]; //Changed this from char* to char[] so the size of the record is always fixed.
     float averageRating;
     int numVotes;
     struct Record* next;
@@ -25,41 +25,62 @@ typedef struct Block {
 
 
 // method declaration
-Record* createRecord(char* line, Record* record);
-void deleteRecord();
+
+void createRecord(char* line, Record* record); 
+
+//added parameters char*id to know which record to delete and block* to specify where to deleted the record from
+void deleteRecord(char*id, Block* block); 
 Block* createBlock(Block* preBlock);
 void deleteBlock();
 void insertBlock(Record* record, Block* block);
 
 
-Record* createRecord(char* line, Record* record, int blockID) {
+void createRecord(char* line, Record* record){
     char* tuple;
+    char delim[2] = "\t";
     strcpy(tuple,line);
-    if(record == NULL)
-    {
-        Record* r1 = malloc(sizeof(Record));
 
-        char* id = strtok(tuple,'\t');
-        strcpy(r1->id, id);
+    char* id = strtok(tuple,delim);
+    strcpy(record->id, id);
 
-        char* averageRating = strtok(NULL,'\t');
-        r1->averageRating = atof(averageRating);
+    char* averageRating = strtok(NULL,delim);
+    record->averageRating = atof(averageRating);
 
-        char* numVotes = strtok(NULL,'\t');
-        r1->numVotes = atoi(numVotes);
+    char* numVotes = strtok(NULL,delim);
+    record->numVotes = atoi(numVotes);
 
-        r1->next = NULL;
-        r1->blockID = blockID;
+    record->next = NULL;
 
-        return r1;
+    record->size = sizeof(Record);
 
-    }else{
-
-    }
+    return;
 }
 
-void deleteRecord() {
+void deleteRecord(char*id, Block* block) {
+    Record* recPtr = block->firstRecord;
+    Record* pre;
 
+    //Case 1: The record to be deleted is the first record.
+    if(strcmp(recPtr->id, id) == 0)
+    {
+        pre = recPtr;
+        recPtr = recPtr->next;
+        block->firstRecord = recPtr;
+        pre->next = NULL;
+        free(pre);
+    }else{ //Case 2: The record to be deleted is the middle or at the end.
+
+        //Travesing to the record to be deleted
+        while(strcmp(recPtr->id, id) != 0)
+        {
+            pre = recPtr;
+            recPtr = recPtr->next;
+        }
+
+        pre->next = recPtr->next;
+        recPtr->next = NULL;
+        free(recPtr);
+    }
 }
 
 Block* createBlock(Block* preBlock) {
@@ -81,6 +102,7 @@ int main(void)
 {
     // load the file
     // Just to note tsv(tab separated values) means they are separated by '\t'(tab)
+    // We probaby need an array to hold all the blocks
     FILE* fp;
     fp = fopen("./data.tsv", "r");
     if (fp == NULL) {
@@ -89,6 +111,7 @@ int main(void)
     }
 
     char buffer[MAX_LEN];
+    
 
     while (fgets(buffer, MAX_LEN, fp))
     {
@@ -96,6 +119,23 @@ int main(void)
         buffer[strcspn(buffer, "\n")] = 0;
         printf("%s\n", buffer);
     }
+    
+    
+    
+    /* 
+    For testing createRecord
+    
+    for(int i = 0; i < 6; i ++){
+        fgets(buffer, MAX_LEN, fp);
+        buffer[strcspn(buffer, "\n")] = 0;
+        if(i > 0)
+        {
+            Record* recPtr = (Record*) malloc(sizeof(Record)); //we have to malloc outside to prevent segementation fault
+            createRecord(buffer, recPtr);
+            printf("Record %d : %s %.2f %d size: %d \n", i, recPtr->id, recPtr->averageRating, recPtr->numVotes, recPtr->size);
+            free(recPtr);
+        }
+    } */
 
     fclose(fp);
     return 0;
