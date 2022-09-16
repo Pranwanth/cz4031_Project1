51e2a00da8021a127832677d20bc0dfb9609e97a
YaxinWan <YWAN008@e.ntu.edu.sg>
2022-09-17 01:50:04 +0800

    finish storage part

--- storage.c
+++ storage.c
@@ -1,12 +1,16 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
 
-#define MAX_LEN 256
+#define MAX_LEN 2560
+#define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+#define PBWIDTH 60
 const int DISK_SIZE = 100000;
-const int BLOCK_SIZE = 8;
+const int BLOCK_SIZE = 200;
+const char* PATH = "./data.tsv";
 
 typedef struct RecordHeader
 {
@@ -17,7 +21,7 @@ typedef struct RecordHeader
 
 typedef struct Record {
     struct  RecordHeader header;
-    char id[10]; //Changed this from char* to char[] so the size of the record is always fixed.
+    char id[11]; //Changed this from char* to char[] so the size of the record is always fixed.
     float averageRating;
     int numVotes;
     struct Record* next;
@@ -48,13 +52,20 @@ int isFull(Block* block, Record* record);
 Block* createBlock(Block* preBlock);
 void deleteBlock(Block** firstBlock, int blockID);
 void printBlock(Block* block);
+Block* getBlock(Block* startBlock, int id);
 Record* getLastRecord(Block* block);
 void insertRecord(Record* record, Block* block);
+Block* run(FILE* fp);
+int getBlockNum(Block* startBlock);
 
+// extra method for better display
+int getLineNum();
+void printProgress(double percentage);
 
 void createRecord(char* line, Record* record){
     char* tuple;
     char delim[2] = "\t";
+    tuple = malloc(sizeof(char) * (strlen(line) + 1));
     strcpy(tuple,line);
 
     char* id = strtok(tuple,delim);
@@ -171,6 +182,30 @@ void printBlock(Block* block) {
     printf("Block: id: %d, capacity: %d, remaining: %d, first record id: %s \n", block->header.id, block->header.capacity, block->header.remainSize, recordID);
 }
 
+Block* getBlock(Block* startBlock, int id) {
+    Block* currentBlock = startBlock;
+    while(currentBlock!=NULL) {
+        if (currentBlock->header.id == id) {
+            return currentBlock;
+        }
+        currentBlock = currentBlock->next;
+    }
+    return NULL;    // unable to find the target block or startBlock is invalid
+}
+
+int getBlockNum(Block* startBlock) {
+    if (startBlock==NULL){
+        return -1;
+    }
+    int blocks = 0;
+    Block* tempBlock = startBlock;
+    while(tempBlock!=NULL) {
+        blocks ++;
+        tempBlock = tempBlock->next;
+    }
+    return blocks;
+}
+
 // parameter: certain block
 // return the last record stored in this block
 Record* getLastRecord(Block* block) {
@@ -199,20 +234,72 @@ void insertRecord(Record* record, Block* block) {
     }
 }
 
+void printProgress(double percentage) {
+    int val = (int) (percentage * 100);
+    int lpad = (int) (percentage * PBWIDTH);
+    int rpad = PBWIDTH - lpad;
+    printf("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
+    fflush(stdout);
+}
+
+Block* run(FILE* fp) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    Record* record;
+    Block* firstBlock;
+    Block* tempBlock = NULL;    // pointer to the current block (last block)
+    
+    read = getline(&line, &len, fp); //get the first line
+    int flag = 0;   // initial flag
+    while ((read = getline(&line, &len, fp)) != -1) {
+        
+        record = (Record*) malloc(sizeof(Record));
+        createRecord(line, record);
+        if (flag==0) {//initial stage
+            firstBlock = createBlock(NULL);
+            tempBlock = firstBlock;
+            flag = 1;
+        } else if (isFull(tempBlock, record)) {// current block is full
+            tempBlock = createBlock(tempBlock);
+        }
+
+        insertRecord(record, tempBlock);
+
+    }
+    free(line);
+
+    return firstBlock;
+}
+
+int getLineNum() {
+    FILE* fp;
+    fp = fopen(PATH, "r");
+    char ch;
+    int lines = 0;
+
+    while(!feof(fp)) {
+        ch = fgetc(fp);
+        if(ch == '\n') {
+            lines++;
+        }
+    }
+    return lines;
+}
+
 int main(void)
 {
     // load the file
     // Just to note tsv(tab separated values) means they are separated by '\t'(tab)
     // We probaby need an array to hold all the blocks
     FILE* fp;
-    fp = fopen("./data.tsv", "r");
+    fp = fopen(PATH, "r");
     if (fp == NULL) {
       perror("Failed: ");
       return 1;
     }
 
-    char buffer[MAX_LEN];
-    
     /*
     while (fgets(buffer, MAX_LEN, fp))
     {
@@ -222,19 +309,10 @@ int main(void)
     }
     */
 
-    //For testing createRecord
-    
-    for(int i = 0; i < 6; i ++){
-        fgets(buffer, MAX_LEN, fp);
-        buffer[strcspn(buffer, "\n")] = 0;
-        if(i > 0)
-        {
-            Record* recPtr = (Record*) malloc(sizeof(Record)); //we have to malloc outside to prevent segementation fault
-            createRecord(buffer, recPtr);
-            printf("Record %d : %s %.2f %d size: %d \n", i, recPtr->id, recPtr->averageRating, recPtr->numVotes, recPtr->header.size);
-            free(recPtr);
-        }
-    }
+    Block* startBLock = run(fp);
+    printf("total number of blocks: %d\n", getBlockNum(startBLock));
+
+    free(startBLock);
 
     fclose(fp);
     return 0;
